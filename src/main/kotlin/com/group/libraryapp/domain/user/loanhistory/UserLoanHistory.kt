6d76eb470fae7748438fc9eb373b7683daf8bb92
kjungw1025 <kjungw1025@naver.com>
2025-05-23 13:16:22 +0900

    refactor: 정적 팩토리 메소드를 통한 유저 대출 현황 조회 코드 리펙토링

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -23,6 +23,9 @@ class UserLoanHistory(
 
     ) {
 
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
